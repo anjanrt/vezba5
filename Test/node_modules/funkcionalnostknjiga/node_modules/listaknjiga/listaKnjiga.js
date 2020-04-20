adaa4acc66917f7ea92853606fc7dd6f4674dfc8
Anja <anjanrt3717@gs.viser.edu.rs>
2020-04-20 17:39:05 +0200

    Poruka

---
+++ Test/node_modules/funkcionalnostknjiga/node_modules/listaknjiga/listaKnjiga.js
@@ -0,0 +1,5 @@
+exports.Lista = [{id:1, naslov:"1", autor:"A"},
+             {id:2, naslov:"2", autor:"B"},
+             {id:3, naslov:"3", autor:"C"},
+             {id:4, naslov:"4", autor:"D"},
+             {id:5, naslov:"5", autor:"E"}];
